f24ae7c3c1e918e191bd9a6e155537349df2f6a6
Ivan Letov <letov88@gmail.com>
2023-04-06 09:10:01 +0300

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -4,7 +4,6 @@ export const movieOptions = {
     // url: 'https://online-movie-database.p.rapidapi.com/auto-complete',
     params: {q: 'batman'},
     headers: {
-      'X-RapidAPI-Key': 'e117fcacabmshf2e2baa8a0278dbp1a3225jsn89c226964580',
       'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
   }
 }
